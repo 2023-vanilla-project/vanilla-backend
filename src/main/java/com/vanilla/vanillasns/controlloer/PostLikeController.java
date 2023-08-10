ccd52eff5acea6e75866217457d709e0dde00d24
chxni <jcg3098@naver.com>
2023-08-11 01:10:47 +0900

    좋아요 취소 기능 추가

--- src/main/java/com/vanilla/vanillasns/controlloer/PostLikeController.java
+++ src/main/java/com/vanilla/vanillasns/controlloer/PostLikeController.java
@@ -1,9 +1,13 @@
 package com.vanilla.vanillasns.controlloer;
 
+import com.vanilla.vanillasns.dto.PostLikeDTO;
 import com.vanilla.vanillasns.entity.Post;
+import com.vanilla.vanillasns.entity.PostLike;
 import com.vanilla.vanillasns.entity.User;
 import com.vanilla.vanillasns.service.PostLikeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -12,14 +16,22 @@ import org.springframework.web.bind.annotation.*;
 public class PostLikeController {
     private final PostLikeService postLikeService;
 
+    //게시글 좋아요
     @PostMapping
-    public void likePost(@PathVariable long postId, @RequestParam String userId) {
-        User user = new User();
-        user.setId(userId);
+    public ResponseEntity<PostLike> addLike(@PathVariable long postId, @RequestParam String userId, @RequestBody PostLikeDTO postLikeDTO) {
+        PostLike postLike = postLikeService.addLike(postId, userId, postLikeDTO);
 
-        Post post = new Post();
-        post.setId(postId);
+        return new ResponseEntity<>(postLike, HttpStatus.CREATED);
+    }
 
-        postLikeService.likePost(user, post);
+    //게시글 좋아요 취소
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteLike(@PathVariable long id) {
+        boolean isDeleted = postLikeService.deleteLike(id);
+        if (isDeleted) {
+            return new ResponseEntity<>("좋아요가 취소되었습니다.", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("좋아요를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/vanilla/vanillasns/service/PostLikeService.java
+++ src/main/java/com/vanilla/vanillasns/service/PostLikeService.java
@@ -1,9 +1,12 @@
 package com.vanilla.vanillasns.service;
 
+import com.vanilla.vanillasns.dto.PostLikeDTO;
 import com.vanilla.vanillasns.entity.Post;
 import com.vanilla.vanillasns.entity.PostLike;
 import com.vanilla.vanillasns.entity.User;
 import com.vanilla.vanillasns.repository.PostLikeRepository;
+import com.vanilla.vanillasns.repository.PostRepository;
+import com.vanilla.vanillasns.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +15,31 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Service
 public class PostLikeService {
-    private final PostLikeRepository postLikeRepository;
 
-    public void likePost(User user, Post post) {
-        Optional<PostLike> existingLike = postLikeRepository.findByUserAndPost(user, post);
-        if (!existingLike.isPresent()) {
-            PostLike postLike = new PostLike();
-            postLike.setUser(user);
-            postLike.setPost(post);
-            postLikeRepository.save(postLike);
+    private final PostLikeRepository postLikeRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public PostLike addLike(long postId, String userId, PostLikeDTO postLikeDTO) {
+        PostLike postLike = new PostLike();
+        postLike.setId(postLikeDTO.getId());
+
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Post no found with ID: " + postId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + userId));
+
+        postLike.setPost(post);
+        postLike.setUser(user);
+
+        return postLikeRepository.save(postLike);
+    }
+
+    public boolean deleteLike(long id) {
+        Optional<PostLike> optionalLike = postLikeRepository.findById(id);
+        if (optionalLike.isPresent()) {
+            postLikeRepository.delete(optionalLike.get());
+            return true;
+        } else {
+            return false;
         }
     }
 }
